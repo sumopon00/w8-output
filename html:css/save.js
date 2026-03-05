37af2c52acbad0e8cace1ff69aabcb376dc61659
sumopon <>
2026-03-05 19:45:32 +0900

    グリッドレイアウト、フォーム自動入力機能　実装

---
+++ html:css/save.js
@@ -0,0 +1,30 @@
+(() => {
+      const form = document.getElementById("js-form");
+      const emailInput = document.getElementById("js-email");
+      const passwordInput = document.getElementById("js-password");
+
+      const STORAGE_KEY = "loginInfo";
+
+      const saveData = localStorage.getItem(STORAGE_KEY);
+
+      if (saveData !== null) {
+            const parsedData = JSON.parse(saveData);
+            emailInput.value = parsedData.email;
+            passwordInput.value = parsedData.password;
+      }
+
+      form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const userData = {
+                  email: emailInput.value,
+                  password: passwordInput.value
+            };
+            
+            const stringifiedData = JSON.stringify(userData);
+
+            localStorage.setItem(STORAGE_KEY, stringifiedData);
+;
+      });
+
+})();
